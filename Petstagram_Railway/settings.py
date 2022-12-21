a1327c06d31ccdbb656113e25ca335d9907a630d
Kamen Stanchev <k.s@abv.bg>
2022-12-21 21:00:22 +0200

    fix dotenv

--- Petstagram_Railway/settings.py
+++ Petstagram_Railway/settings.py
@@ -11,15 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-
-
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
-# SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
